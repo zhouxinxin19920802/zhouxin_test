ef469eea428ea65e9269ee48f2379faa090d1172
75247 <1470328933@qq.com>
2023-03-08 19:31:27 +0800

    lianxi1

--- test2/test3/zzz.py
+++ test2/test3/zzz.py
@@ -7,4 +7,10 @@
 
 from ffff1 import Employee1
 
-
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter as sg
+def data_read(shape_data, data):
+    data_shape_after = []
+    for line in open(shape_data, "r", encoding="utf-8"):
+        line = line.strip("\n")
+        data_shape_after.append(float(line))
